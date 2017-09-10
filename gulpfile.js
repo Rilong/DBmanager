1c959716f1650a1ad68a8e977b8283b4f4fb0048
Rilong <romgnatyuk@gmail.com>
2017-09-10 19:57:21 +0300

    Inited project

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    autoPrefixer = require("gulp-autoprefixer");
+    browserSync = require("browser-sync"),
+    uncss = require("gulp-uncss"),
+    uglify = require("gulp-uglify"),
+    rename = require("gulp-rename"),
+    concat = require("gulp-concat"),
+    cssnano = require("gulp-cssnano"),
+    del = require("del");
+
+gulp.task("sass", function () {
+    return gulp.src("app/sass/*.sass")
+            .pipe(sass({outputStyle: "expanded"}))
+            .pipe(autoPrefixer({browsers: ['last 15 versions', '> 1%', 'IE 8', 'IE 7'], cascade: true}))
+            .pipe(gulp.dest("app/css"))
+            .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task("cssLibs", ["sass"], function () {
+    return gulp.src("app/css/libs.css")
+        .pipe(cssnano())
+        .pipe(rename({suffix: ".min"}))
+        .pipe(gulp.dest("app/css"))
+});
+
+gulp.task("browserSync", function () {
+   browserSync({
+       server: {
+           baseDir: "app/"
+       },
+       notify: false
+   })
+});
+
+gulp.task("scripts", function () {
+   return gulp.src(
+       ["app/libs/jquery/jquery.min.js",
+        "app/libs/bootstrap/js/bootstrap.min.js"
+       ])
+       .pipe(uglify())
+       .pipe(concat("libs.js"))
+       .pipe(gulp.dest("app/js"));
+});
+
+gulp.task("default", ["cssLibs", "scripts", "browserSync"],function () {
+    gulp.watch("app/sass/**/*.sass", ["sass"]);
+    gulp.watch(["app/**/*.html", "app/js/**/*.js"], browserSync.reload)
+});
