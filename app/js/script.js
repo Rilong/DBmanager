1f4f5750d5b3ad8a821469ff01f4a404df7ab5b7
Rilong <romgnatyuk@gmail.com>
2017-09-14 18:05:41 +0300

    Created table list page

--- app/js/script.js
+++ app/js/script.js
@@ -12,6 +12,20 @@ $(function () {
 
     });
 
+    $('.table-remove').magnificPopup({
+        items: {
+            src: '#alert-popup',
+            closeBtnInside:true,
+            type: 'inline'
+        },
+
+        closeMarkup: "<button title=\"Close (Esc)\" type=\"button\" class=\"mfp-close icon\"></button>",
+        removalDelay: 800,
+        mainClass: 'mfp-scale'
+
+    });
+
+
     $(".input-check > input").iCheck({
         checkboxClass: 'icheckbox_styling',
         radioClass: 'iradio_styling'
